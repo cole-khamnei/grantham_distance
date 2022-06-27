42d75a6e2f5d058151e9b1e2d724867435ab8a76
Cole Khamnei <cole.khamnei@gmail.com>
2022-06-27 16:54:51 -0400

    added cover plot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # grantham_distance
 Python tool for calculating grantham distance.  Sort of a port but not really a port of GranthamDist (https://sourceforge.net/projects/granthamdist/)
+
+![GitHub Logo](/cover_plots/HLA_loci_distances.png)
--- constants.py
+++ constants.py
@@ -8,6 +8,7 @@ import pathlib
 GRANTHAM_DISTANCE_DIR_PATH = pathlib.Path(__file__).parent.resolve()
 
 RESOURCES_DIR_PATH = os.path.join(GRANTHAM_DISTANCE_DIR_PATH, "resources")
+COVER_PLOTS_PATH = os.path.join(GRANTHAM_DISTANCE_DIR_PATH, "cover_plots")
 
 HLA_CLASS_I_ALLELE_AMINO_ACID_SEQUENCES_FULL_PATH = os.path.join(RESOURCES_DIR_PATH, "HLA_class_I_allele_AA_sequences.fas")
 HLA_CLASS_I_ALLELE_AMINO_ACID_SEQUENCES_ABS_PATH = os.path.join(RESOURCES_DIR_PATH, "HLA_class_I_allele_AA_sequences_ABS.fas")
