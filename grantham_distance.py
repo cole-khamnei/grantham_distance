a65d44ad7c09a71c9811553bae0dbe6c5a273005
Cole Khamnei <cole.khamnei@gmail.com>
2022-07-07 23:05:10 -0400

    routine

--- grantham_distance.py
+++ grantham_distance.py
@@ -12,7 +12,7 @@ import pandas as pd
 class HLAAlleleSequenceMapping:
     def __init__(self, HLA_fas_path: str):
         self.load_HLA_allele_AA_mapping(HLA_fas_path)
-        
+
     def parse_fas_section(self, section: str):
         """"""
         assert ">" not in section
@@ -29,23 +29,22 @@ class HLAAlleleSequenceMapping:
     def _map(self, alleles: str):
         if isinstance(alleles, str):
             return self.HLA_allele_AA_mapping.get(alleles, None)
-        
-        return [self._map(allele) for allele in alleles]
 
+        return [self._map(allele) for allele in alleles]
 
 
 class GranthamDistance:
     def __init__(self, grantham_AA_table_path: str):
         self.AA_distance_table = pd.read_csv(grantham_AA_table_path, sep='\t', index_col=0)
-        
+
         self.AA_distance_dict = {}
         for i in self.AA_distance_table.index:
             for j in self.AA_distance_table.columns:
                 self.AA_distance_dict[i + j] = self.AA_distance_table.loc[i, j]
-    
+
     def __repr__(self):
-        return str(grantham_distance.AA_distance_table)
-    
+        return str(self.AA_distance_table)
+
     def AA_pair_distance(self, AA_1: str, AA_2: str) -> int:
         """ """
         return self.AA_distance_dict.get(AA_1.upper() + AA_2.upper(), None)
@@ -57,15 +56,15 @@ class GranthamDistance:
 
         assert len(AA_sequence_1) == len(AA_sequence_2), f""
         normalize_length = len(AA_sequence_1)
-        
+
         distances = [self.AA_pair_distance(AA_1, AA_2) for AA_1, AA_2 in zip(AA_sequence_1, AA_sequence_2)
                      if AA_1 != AA_2]
-        
+
         if any(dist is None for dist in distances):
             return None
-        
+
         return np.sum(distances) / normalize_length
-    
+
     def sequence_group_distance(self, sequences: list, mean: bool = True) -> float:
         """ Mean of all the pairwise distances"""
 
@@ -73,7 +72,7 @@ class GranthamDistance:
         distances = [self.sequence_pair_distance(*pair) for pair in sequence_pairs]
         if any(dist is None for dist in distances):
             return None
-        
+
         return np.mean(distances)
 
 
